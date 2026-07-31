3898c829c80f71e549beabaa2922503da62f8a24
engarambe-spec <engarambe-spec@users.noreply.github.com>
2026-07-30 17:04:57 -0700

    Fix: use BINARY for case-sensitive name match in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], charset="utf8")
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4])
+    query = "SELECT * FROM states WHERE BINARY name = '{}' " \
+        "ORDER BY id ASC".format(sys.argv[4])
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
