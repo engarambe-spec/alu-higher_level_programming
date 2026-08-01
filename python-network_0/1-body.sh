07ef287e4e70a47466652e1f172acca3ce32e2d3
engarambe-spec <engarambe-spec@users.noreply.github.com>
2026-08-01 03:21:53 -0700

    Fix 1-body.sh to follow redirects with -L

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to a URL and displays the body, only for a 200 status
-[ "$(curl -s -o /tmp/1-body_response -w '%{http_code}' "$1")" = "200" ] && cat /tmp/1-body_response
+[ "$(curl -s -L -o /tmp/1-body_response -w '%{http_code}' "$1")" = "200" ] && cat /tmp/1-body_response
